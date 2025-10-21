f8ece70d0d7292a41c7375af496d1fc8fb69f522
Ilias Belghoul <ilias.belghoul@gmail.com>
2025-10-21 11:18:40 +0200

    add roman to integer task

---
+++ src/main/java/org/example/leetcode/RomantoInteger13/Main.java
@@ -0,0 +1,10 @@
+package org.example.leetcode.RomantoInteger13;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "MCMXCIV";
+
+        System.out.println(solution.romanToInt(s));
+    }
+}
